2e5a43d225f9a592711082131b723b937c3ae20c
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-08 10:25:16 +0100

    IAM role and permissions

--- iam.tf
+++ iam.tf
@@ -1,49 +1,29 @@
-resource "aws_iam_role" "lambda_exec" {
-  name = "LambdaEC2ExecutionRole"
+# Create IAM role for Lambda to execute with basic Lambda trust policy
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_logs" {
-  name       = "attach-lambda-basic-execution"
-  roles      = [aws_iam_role.lambda_exec.name]
+# Attach EC2 Full Access so Lambda can start/stop instances
+resource "aws_iam_policy_attachment" "lambda_ec2_access" {
+  name       = "lambda_ec2_policy_attach"
+  roles      = [aws_iam_role.lambda_exec_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+# Attach basic execution role so Lambda can write to CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_policy" "ec2_control" {
-  name        = "LambdaEC2ControlPolicy"
-  description = "Allow Lambda to start/stop EC2"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2:StartInstances",
-          "ec2:StopInstances",
-          "ec2:DescribeInstances"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "ec2_control_attach" {
-  name       = "attach-ec2-control"
-  roles      = [aws_iam_role.lambda_exec.name]
-  policy_arn = aws_iam_policy.ec2_control.arn
-}
