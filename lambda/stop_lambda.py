29f89899b0fbd541cfa01e888c64bd4e52bf8b40
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-08 10:55:33 +0100

    lambda folder updates

---
+++ lambda/stop_lambda.py
@@ -0,0 +1,9 @@
+import boto3
+import os
+
+client = boto3.client('ec2')
+instance_id = os.environ['INSTANCE_ID']
+
+def lambda_handler(event, context):
+    response = client.stop_instances(InstanceIds=[instance_id])
+    print(response)
