43ed057be9311ab2bd399d1d9928571b2d64a442
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-08 10:27:07 +0100

    vpc configuration

---
+++ vpc.tf
@@ -0,0 +1,50 @@
+# Create a custom VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "autoec2-vpc"
+  }
+}
+
+# Create a public subnet in the VPC
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "autoec2-subnet"
+  }
+}
+
+# Internet Gateway to allow internet access
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "autoec2-igw"
+  }
+}
+
+# Route table for internet access
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "autoec2-rt"
+  }
+}
+
+# Associate the route table with the public subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
