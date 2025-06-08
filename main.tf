26a3e1b37921456e4ede7ce8ee8e8e581f3a3dfe
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-08 10:14:34 +0100

    main.tf updates

--- main.tf
+++ main.tf
@@ -1,23 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-1a"
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH"
+# Create EC2 security group
+resource "aws_security_group" "autoec2_sg" {
+  name        = "autoec2-sg"
+  description = "Allow SSH inbound"
   vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # for demo; restrict in production
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -26,118 +17,21 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-
-resource "aws_instance" "lamda_ec2" {
-  ami = "ami-03400c3b73b5086e9"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "AutoEC2"
+    Name = "autoec2-sg"
   }
 }
 
-# Create s3 bucket to store lambda function
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "ec2-lambda-functions-bucket-${random_id.suffix.hex}"
-  force_destroy = true
-}
+# Create EC2 instance
+resource "aws_instance" "auto_ec2" {
+  ami                    = "ami-03400c3b73b5086e9"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.autoec2_sg.id]
+  associate_public_ip_address = true
 
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-# Upload start Lambda
-resource "aws_lambda_function" "start_ec2" {
-  function_name = "StartEC2Instance"
-  s3_bucket     = aws_s3_bucket.lambda_bucket.id
-  s3_key        = aws_s3_object.start_lambda.key
-  handler       = "start_ec2.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec.arn
-  timeout       = 10
-
-  environment {
-    variables = {
-      INSTANCE_ID = aws_instance.lamda_ec2.id
-    }
+  tags = {
+    Name = var.instance_name
   }
-}
-
-# Upload stop Lambda
-resource "aws_lambda_function" "stop_ec2" {
-  function_name = "StopEC2Instance"
-  s3_bucket     = aws_s3_bucket.lambda_bucket.id
-  s3_key        = aws_s3_object.stop_lambda.key
-  handler       = "stop_ec2.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec.arn
-  timeout       = 10
-
-  environment {
-    variables = {
-      INSTANCE_ID = aws_instance.lamda_ec2.id
-    }
-  }
-}
-
-#Upload start Lambda function file
-resource "aws_s3_object" "start_lambda" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "start_ec2.zip"
-  source = "${path.module}/lambda/start_ec2.zip"
-}
-
-# Upload Stop Lambda function file
-resource "aws_s3_object" "stop_lambda" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "stop_ec2.zip"
-  source = "${path.module}/lambda/stop_ec2.zip"
-}
-
-# Configure CloudWatch Start event (Schedule rule)
-resource "aws_cloudwatch_event_rule" "start_event" {
-  name                = "StartEC2Daily"
-  schedule_expression = "cron(0 9 * * ? *)" # 9:00 AM UTC
-}
-
-# Configure CloudWatch stop event (Schedule rule)
-resource "aws_cloudwatch_event_rule" "stop_event" {
-  name                = "StopEC2Daily"
-  schedule_expression = "cron(0 17 * * ? *)" # 5:00 PM UTC
-}
-
-# Trigger lambda start function (Event targets)
-resource "aws_cloudwatch_event_target" "start_lambda_trigger" {
-  rule      = aws_cloudwatch_event_rule.start_event.name
-  target_id = "StartEC2Target"
-  arn       = aws_lambda_function.start_ec2.arn
-}
-
-# Trigger lambda stop function (Event targets)
-resource "aws_cloudwatch_event_target" "stop_lambda_trigger" {
-  rule      = aws_cloudwatch_event_rule.stop_event.name
-  target_id = "StopEC2Target"
-  arn       = aws_lambda_function.stop_ec2.arn
-}
-
-# Grant CloudWatch events permission to invoke start function
-resource "aws_lambda_permission" "allow_cloudwatch_start" {
-  statement_id  = "AllowExecutionFromCloudWatchStart"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.start_ec2.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.start_event.arn
-}
-
-# Grant CloudWatch events permission to invoke stop function
-resource "aws_lambda_permission" "allow_cloudwatch_stop" {
-  statement_id  = "AllowExecutionFromCloudWatchStop"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.stop_ec2.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.stop_event.arn
 }
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "instance_name" {
+  default = "AutoEC2"
+}
